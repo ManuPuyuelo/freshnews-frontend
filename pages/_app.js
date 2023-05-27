f63c87becb8bf2e13f40cef61c9bd1e65a83d420
Manu Puyuelo <puyuelo.m@gmail.com>
2023-05-27 12:32:34 +0200

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,26 @@
-import '../styles/globals.css';
-import Head from 'next/head';
-import Header from '../components/Header';
+import "../styles/globals.css";
+import Head from "next/head";
+import Header from "../components/Header";
 
 // redux imports
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import bookmarks from '../reducers/bookmarks';
-import user from '../reducers/user';
-import hiddenArticles from '../reducers/hiddenArticles';
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import bookmarks from "../reducers/bookmarks";
+import user from "../reducers/user";
+import hiddenArticles from "../reducers/hiddenArticles";
 
 // redux-persist imports
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
 
 const reducers = combineReducers({ bookmarks, user, hiddenArticles });
-const persistConfig = { key: 'morningnews', storage };
+const persistConfig = { key: "freshnews", storage };
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, reducers),
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 const persistor = persistStore(store);
@@ -29,7 +30,7 @@ function App({ Component, pageProps }) {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Head>
-          <title>Morning News</title>
+          <title>FreshNews</title>
         </Head>
         <Header />
         <Component {...pageProps} />
